d4fbc22cdfab9472209d3a1b038deef5e7072cf3
syam <syam@ios.com>
2021-04-03 18:01:22 +0530

    we set constraints for segmentControl

--- Git-Sample/Git-Sample/ViewController.swift
+++ Git-Sample/Git-Sample/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("welcome to git")
         // Do any additional setup after loading the view.
     }
 
